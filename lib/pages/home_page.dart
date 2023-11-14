788d56bbcec7d3465c805a2eae05c8f43f493eef
Fathir <muhammadfathir5798@gmail.com>
2023-11-14 14:40:48 +0700

    custom component

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,22 +15,33 @@ class HomePage extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Container(
-            margin: EdgeInsets.all(8),
-            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-            decoration: BoxDecoration(
-              color: Colors.greenAccent
-            ),
-            child: Text("Persegi"),
-          ),
-          Container(
-            margin: EdgeInsets.all(8),
-            padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-            decoration: BoxDecoration(
-                color: Colors.greenAccent
-            ),
-            child: Text("Segitiga"),
-          )
+          CustomMenu(imageAssets: "assets/Persegi.png",title: "Persegi"),
+          CustomMenu(imageAssets: "assets/Segitiga.png",title: "Segitiga"),
+          CustomMenu(imageAssets: "assets/persegipanjang.png",title: "Persegi Panjang"),
+          CustomMenu(imageAssets: "assets/lingkaran.png",title: "Lingkaran"),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomMenu extends StatelessWidget {
+  const CustomMenu({
+    super.key, required this.imageAssets, required this.title,
+  });
+  final String imageAssets;
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(8),
+      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      decoration: BoxDecoration(color: Colors.greenAccent),
+      child: Column(
+        children: [
+          Image.asset(imageAssets, height: 100, width: 100),
+          Text(title),
         ],
       ),
     );
